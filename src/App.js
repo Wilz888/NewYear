78f44b37b77e48d53dc17c4d44f980e7b0081572
Wilz888 <115535731+Wilz888@users.noreply.github.com>
2023-12-30 21:41:46 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import Particles from "react-particles";
+import React, { useEffect, useState } from "react";
+import { loadFireworksPreset } from "tsparticles-preset-fireworks";
+import { Typewriter } from "react-simple-typewriter";
+import Countdown from "react-countdown";
+
+function App() {
+  const [newYearMessage, setNewYearMessage] = React.useState(["Bye 2023! Hope 2024 Will Be Better"]);
+  const [newMessage, setNewMessage] = React.useState(["Hello Kristy Veronica👋"]);
+  const [newGombalan, setNewGombalan] = React.useState(["I Hope You Like It😁"]);
+  const [newMundur, setMundur] = React.useState(["👇Countdown👇"]);
+
+  const particlesInitialization = async (engine) => {
+    await loadFireworksPreset(engine);
+  }
+
+  function timeLeft() {
+    const newYearDate = new Date("January 1, 2024 00:00:").getTime()
+    const nowDate = new Date().getTime()
+    const remainingTime = newYearDate - nowDate
+    return remainingTime
+  }
+
+  return (
+    <>
+      <Particles
+        init={particlesInitialization}
+        options={{ preset: "fireworks" }}
+      />
+      <div className="flex flex-col justify-center items-center min-h-screen gap-5 font-bold">
+        <div className="z-50 text-white font-bold text-4xl">
+          <Typewriter
+            words={newMessage}
+            cursorStyle={"_"}
+            cursor
+          />
+        </div>
+        <span className="text-white text-2xl font-bold z-50">
+          <Typewriter words={newGombalan} 
+          cursorStyle={"_"} 
+          cursor />
+        </span>
+        <span className="text-white text-4xl font-bold px-10 z-50">
+          <Typewriter
+            words={newYearMessage}
+            loop={false}
+            cursorStyle={"👋_"}
+            cursor
+          />
+        </span>
+        <div className="z-50 text-white">
+          <span className="text-white text-2xl z-50">
+            <Typewriter words={newMundur} />
+          </span>
+        </div>
+        <div className="z-50 text-white font-bold text-2xl">
+          <Countdown
+            date={Date.now() + timeLeft()}
+            onComplete={() =>
+              setNewYearMessage(["Happy", "New", "Year", "HNY 2024"])
+            }
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
