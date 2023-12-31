1ca0b6e65382439a4b639b545867f483ff0c042f
Wilz888 <115535731+Wilz888@users.noreply.github.com>
2023-12-31 08:28:32 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,20 +5,24 @@ import { Typewriter } from "react-simple-typewriter";
 import Countdown from "react-countdown";
 
 function App() {
-  const [newYearMessage, setNewYearMessage] = React.useState(["Bye 2023! Hope 2024 Will Be Better"]);
-  const [newMessage, setNewMessage] = React.useState(["Hello Kristy Veronica👋"]);
-  const [newGombalan, setNewGombalan] = React.useState(["I Hope You Like It😁"]);
-  const [newMundur, setMundur] = React.useState(["👇Countdown👇"]);
+  const [newYearMessage, setNewYearMessage] = React.useState([
+    "Bye 2023! Hope 2024 Will Be Better",
+  ]);
+  const [newMessage, setNewMessage] = React.useState([
+    "Hello Kristy Veronica👋",
+  ]);
+  const [newGombalan, setNewGombalan] = React.useState(["Hope You Like It😁"]);
+  const [newMundur, setMundur] = React.useState(["👇Countdown to 2024👇"]);
 
   const particlesInitialization = async (engine) => {
     await loadFireworksPreset(engine);
-  }
+  };
 
   function timeLeft() {
-    const newYearDate = new Date("January 1, 2024 00:00:").getTime()
-    const nowDate = new Date().getTime()
-    const remainingTime = newYearDate - nowDate
-    return remainingTime
+    const newYearDate = new Date("January 1, 2024 00:00:00").getTime();
+    const nowDate = new Date().getTime();
+    const remainingTime = newYearDate - nowDate;
+    return remainingTime;
   }
 
   return (
@@ -29,22 +33,16 @@ function App() {
       />
       <div className="flex flex-col justify-center items-center min-h-screen gap-5 font-bold">
         <div className="z-50 text-white font-bold text-4xl">
-          <Typewriter
-            words={newMessage}
-            cursorStyle={"_"}
-            cursor
-          />
+          <Typewriter words={newMessage} cursorStyle={"_"} cursor />
         </div>
         <span className="text-white text-2xl font-bold z-50">
-          <Typewriter words={newGombalan} 
-          cursorStyle={"_"} 
-          cursor />
+          <Typewriter words={newGombalan} cursorStyle={"_"} cursor />
         </span>
-        <span className="text-white text-4xl font-bold px-10 z-50">
+        <span className="text-white text-2xl font-bold px-10 z-50">
           <Typewriter
             words={newYearMessage}
             loop={false}
-            cursorStyle={"👋_"}
+            cursorStyle={"_"}
             cursor
           />
         </span>
